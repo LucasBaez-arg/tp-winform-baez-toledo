e47bae3426932b58e714b1c3e01a823c7470ff35
Wmatias16 <wmatias1009@gmail.com>
2021-04-26 22:00:22 -0300

    Categoria Negocio

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<Categoria> lista = new List<Categoria>();
+            try
+            {
+                datos.SetearConsulta("SELECT id,descipcion FROM CATEGORIAS");
+                datos.EjecutarLectura();
+                while (datos.Leer.Read())
+                {
+                    lista.Add(new Categoria((int)datos.Leer["id"], (string)datos.Leer["descripcion"]));
+                }
+
+                return lista;
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            finally
+            {
+                datos.CerraConexion();
+            }
+        }
+    }
+}
